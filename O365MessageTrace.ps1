4e44cfc3647ae402c6c56de31a16cfee07d3a650
Alexandru Liviu Nita <45991397+alexandrunita@users.noreply.github.com>
2023-08-16 10:38:36 +0300

    SummaryReport Export

--- O365MessageTrace.ps1
+++ O365MessageTrace.ps1
@@ -1,40 +1,57 @@
-function Connect-O365EXOPS
+<#
+.SYNOPSIS
+This script provides options for collecting Summary/Detailed Message Trace information from EXO.
+Before running the script, you need to be connected to Exchange Online Powershell:
+https://learn.microsoft.com/powershell/exchange/connect-to-exchange-online-powershell?view=exchange-ps
+#>
+using namespace System.Collections.Generic
+
+[CmdletBinding()]
+param (
+    [Parameter(Mandatory = $false)][string]$SenderAddress,
+    [Parameter(Mandatory = $false)][datetime]$StartDate,
+    [Parameter(Mandatory = $false)][datetime]$EndDate,
+    [Parameter(Mandatory = $false)][Int16]$NumberOfDaysInPast,
+    [Parameter(Mandatory = $false)][bool]$DetailedReport = $false
+)
+
+function Get-SummaryReport
 {
-    $Error.Clear()
-
-    Set-ExecutionPolicy RemoteSigned
-
-    $O365GlobalAdminCredentials = Get-Credential
-
-    $O365EXOSession = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://outlook.office365.com/powershell-liveid/ -Credential $O365GlobalAdminCredentials -Authentication Basic -AllowRedirection -ErrorAction Continue
-
-    Import-PSSession $O365EXOSession -AllowClobber
-}
-
-function Get-O365MessageTrace
-{
+    param (
+        [Parameter(Mandatory = $true)][datetime]$StartDate,
+        [Parameter(Mandatory = $true)][datetime]$EndDate,
+        [Parameter(Mandatory = $false)][string]$SenderAddress
+    )
+    # initialize Empty generic list
+    $SummaryReport = [List[PSObject]]::new()
     $i = 1
-    do
+    while($true)
     {
         Write-Host "Collecting information from MessageTrace Trace Page $i"
 
-        $CurrentMessageTrace = Get-MessageTrace -StartDate $StartDate -EndDate $EndDate -PageSize 5000 -Page $i
+        $CurrentMessageTrace = Get-MessageTrace -SenderAddress $SenderAddress -StartDate $StartDate -EndDate $EndDate -PageSize 5000 -Page $i
 
-        $SummaryReport += $CurrentMessageTrace
+        # if message trace results not null, add them to List, else break out of the loop
+        if($null -ne $CurrentMessageTrace) {
+            [void]$SummaryReport.Add($CurrentMessageTrace)
+        }
+        else {break}
         $i++
 
         Start-Sleep -m 500
     }
-    while($null -ne  $CurrentMessageTrace)
 
-    $SummaryReport | Select-Object -Property Received, SenderAddress, RecipientAddress, Subject, MessageId, MessageTraceId, Status, ToIP, FromIP, Size | export-csv "$LogPath\SummaryReport.csv" -NoTypeInformation -Append
+    # The result is an List of Arrays, we will need to export all of these to local file
+    foreach($Report in $SummaryReport) {
+        $Report| export-csv "$LogPath\SummaryReport.csv" -NoTypeInformation -Append
+    }
+    
+    Write-Host "Exported Get-MessageTrace output to $LogPath\SummaryReport.csv"
 
     return $SummaryReport
-
-    Write-Host "Exported Get-MessageTrace output to $LogPath\SummaryReport.csv"
 }
 
-function Get-O365MessageTraceDetail($SummaryReport)
+function Get-SummaryReportDetail($SummaryReport)
 {
     Write-Host -ForegroundColor Yellow "We have $SummaryReport.Count Get-MessageTraceDetail operations to run
     Do you want to run Get-MessageTraceDetail and export CSV output?
@@ -73,28 +90,31 @@ function Get-O365MessageTraceDetail($SummaryReport)
 
         Default 
         {
-            Get-O365MessageTraceDetail($SummaryReport)
+            Get-SummaryReportDetail($SummaryReport)
         }
     }
 }
 
-
+################################################################################################
 # Main Script Section
-###Calls EXO Connection function
-###Collects Get-MessageTrace & Get-MessageTraceDetail output
-###Writes Log files on Desktop
 
+#Create Log File Directory on Desktop
 $ts = Get-Date -Format yyyyMMdd_HHmm
-$LogPath=[Environment]::GetFolderPath("Desktop")+"\$($ts)_MessageTrace"
+$LogPath=[Environment]::GetFolderPath("Desktop")+"\MessageTraceScript\$($ts)_MessageTrace"
 Write-Host "Created Directory on Desktop:"
 mkdir "$LogPath"
 
-$StartDate = ([DateTime]::UtcNow.AddDays(-10))
-$EndDate = ([DateTime]::UtcNow)
+#If no Start/End Dates provided by end user, default to StartDate 2 days ago and EndDate now
+if($StartDate -eq $null) {
+    $StartDate = ([DateTime]::UtcNow.AddDays(-2))
+}
+if($EndDate -eq $null) {
+    $EndDate = ([DateTime]::UtcNow)
+}
 
 $SummaryReport = $null
 $MTDReport = $null
 
-Connect-O365EXOPS
-$SummaryReport = Get-O365MessageTrace
-Get-O365MessageTraceDetail($SummaryReport)
+#Collect Summary Report
+$SummaryReport = Get-SummaryReport -SenderAddress $SenderAddress -StartDate $StartDate -EndDate $EndDate
+#Check if DetailedReport requested, and collect
